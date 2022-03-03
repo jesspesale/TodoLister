53f804c08ef783f25a5f437e56889bd46d452374
jesspesale <jess_pesale@aol.com>
2022-03-02 22:09:16 -0500

    added edit state in todo comp

---
+++ src/componenets/Todo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import TodoForm from './TodoForm'
+
+function Todo() {
+
+    const [edit, setEdit] = useState({id: null, value: ''})
+
+  return (
+    <div>Todo</div>
+  )
+}
+
+export default Todo
