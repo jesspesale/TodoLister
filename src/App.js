1b8bb24040d1f9d1830a65df55cd3ec35d5ef370
jesspesale <jess_pesale@aol.com>
2022-03-02 21:49:16 -0500

    started todo form and handle submit function

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import {TodoForm} from "./componenets/TodoForm"
 
 
 function App() {
--- src/componenets/TodoForm.js
+++ src/componenets/TodoForm.js
@@ -5,16 +5,21 @@ function TodoForm() {
 
     const [input, setInput] = useState('')
 
+    handleSubmit = (e) => {
+      e.preventDefault()
+    }
+
   return (
     <div>
-        <form className="todo-form">
+        <form className="todo-form" onSubmit={this.handleSubmit}>
             <input
               type="text"
               placeholder="Add a todo"
               value={input}
               className="todo-input"
             />
-
+            <button className='todo-button'>Add todo</button>
+        </form>
     </div>
   )
 }
