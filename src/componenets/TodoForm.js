7f0d8ddda363d6cd3df790e74b209431ffbc739a
jesspesale <jess_pesale@aol.com>
2022-03-02 22:07:52 -0500

    adding todos works now just have to display them

--- src/componenets/TodoForm.js
+++ src/componenets/TodoForm.js
@@ -11,9 +11,10 @@ function TodoForm(props) {
     const handleSubmit = (e) => {
       e.preventDefault()
 
-      // props.onSubmit({
-      //   id: Math.floor(Math.random() * 1000000)
-      // })
+      props.onSubmit({
+        id: Math.floor(Math.random() * 1000000),
+        text: input
+      })
 
       setInput("")
     }
--- src/componenets/TodoList.js
+++ src/componenets/TodoList.js
@@ -5,21 +5,20 @@ import TodoForm from "./TodoForm"
 function TodoList() {
     const [todos, setTodos] = useState([])
 
-    const addTodo = () => {
-        if(!todos.text || /^\s*$/.test(todo.text)){
+    const addTodo = (todo) => {
+        if(!todo.text || /^\s*$/.test(todo.text)){
             return
         }
         const newTodos = [todo, ...todos]
 
         setTodos(newTodos)
-        console.log(...todos)
     }
 
 
   return (
     <div>
         <h2>What do we have to do today?</h2>
-        <TodoForm/>
+        <TodoForm onSubmit={addTodo}/>
     </div>
   )
 }
