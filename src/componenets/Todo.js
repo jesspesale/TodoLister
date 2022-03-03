feb1362dba045b33499feeedec1770a8d5d8af91
jesspesale <jess_pesale@aol.com>
2022-03-02 22:12:08 -0500

    added ternary to show if todo is complete or not

--- src/componenets/Todo.js
+++ src/componenets/Todo.js
@@ -6,7 +6,16 @@ function Todo() {
     const [edit, setEdit] = useState({id: null, value: ''})
 
   return (
-    <div>Todo</div>
+    <div>
+        {todos.map(todo => (
+            <div 
+                className={todo.isComplete ? 'todo-row complete' : 'todo-row'} 
+                key={index}
+            >
+
+            </div>
+        ))}
+    </div>
   )
 }
 
